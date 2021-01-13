33ad61d9e714c0ad9bfe42a1246012586fc08285
Maarten Wolfsen <maartenwolfsen@gmail.com>
2021-01-13 13:30:51 +0100

    Add homepage

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include "./partials/head.php" ?>
+    <body class="home">
+        <div class="content">
+            <img class="logo"
+                 src="images/logo_trans_text-big.png"
+                 alt="15 Days Of Code Logo">
+            <ul class="menu">
+                <li><a href="/" title="Play Game">Play Game</a></li>
+                <li><a href="/" title="About">About</a></li>
+                <li><a href="/" title="Report a Bug">Report a Bug</a></li>
+            </ul>
+        </div>
+        <?php include "./partials/footer.php" ?>
+    </body>
+</html>

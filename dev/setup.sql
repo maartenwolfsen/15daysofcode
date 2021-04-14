d771071ffa5676ced4fc9b88f84c1504cd1efd3b
Maarten Wolfsen <maartenwolfsen@gmail.com>
2021-04-14 11:13:00 +0200

    Change table name

--- dev/setup.sql
+++ dev/setup.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS `users` (
     PRIMARY KEY (`id`)
     ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa', 'test@test.com');
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (1, 'test', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa', 'test@test.com');
